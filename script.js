451fa065da7b81bea823d61b01a204578764fa52
Ewijah <edgordon2020@gmail.com>
2023-04-30 20:19:36 -0400

    create play round and game functions

--- script.js
+++ script.js
@@ -1,39 +1,69 @@
-function getComputerChoice() {
-    let arrChoices = ["rock", "paper", "scissors"]
 
-     let randomize = arrChoices[Math.floor(Math.random() * 3)];
-     return randomize;
-}
-function playRound(playerSelection, computerSelection){
-   playerSelection
 
-    if (playerSelection == computerSelection){
-        return  "draw!";
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+ const choice = options[Math.floor(Math.random() * options.length)];
+ return choice;
 }
-    else if (playerSelection == "rock" && computerSelection == "paper"){
-        return "you lose, paper beats rock!";
+
+
+
+function checkWinner(playerSelection, computerSelection) {
+ if (playerSelection == computerSelection) {
+    return "Tie"
+ }
+ else if(
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper") 
+ ) {
+    return "Player";
+ }
+ else {
+    return "Computer";
+}
+
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result == "Tie") {
+        return "It's a tie!"
     }
-    else if (playerSelection == "rock" && computerSelection == "scissors"){
-        return "you win, rock beats scissors!";
+    else if (result == "Player"){
+        return `You win! ${playerSelection} beats ${computerSelection}`
     }
-    else if (playerSelection == "paper" && computerSelection == "scissors"){
-        return "you lose, scissors beats paper!";
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
     }
-    else if (playerSelection == "paper" && computerSelection == "rock")
-    {
-        return "you lose, rock beats paper!";
+    
+
+}
+
+function getPlayerChoice() {
+    let validatedInput = false;
+    while (validatedInput == false){
+        const choice = prompt("rock, paper, or scissors?");
+        if (choice == null) {
+            continue;
         }
-    else if (playerSelection == "scissors" && computerSelection == "paper"){
-        return "you win, scissors beats paper!";
+        const choiceLowercase = choice.toLowerCase();
+        if (options.includes(choiceLowercase)){
+            validatedInput = true;
+            return choiceLowercase;
+        }
     }
-    else if (playerSelection == "scissors" && computerSelection == "rock"){
-        return "you lose, rock beats scissors!";
-    }
-
-
 }
 
+function game(){
 
-const playerSelection = prompt("rps?")
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game()
